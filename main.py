9b745810a44d3199a4a948386939915fad90c5d6
swaruppanda11 <swaruppanda11@gmail.com>
2025-10-27 14:32:10 -0600

    prompt and response tokens printed

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
+def main(): 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum'
+    )
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents = 'Why does the universe keep expanding and what will happen in the future? 2 paragraphs.'
-)
+    print(response.text)
 
-print(response.text)
+    if response.usage_metadata is None or response is None:
+        print("Response is Null")
+        return
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+main()
