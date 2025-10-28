4f23d40e902b42acc3ea80fe76b46d54c169a1a0
swaruppanda11 <swaruppanda11@gmail.com>
2025-10-28 15:46:32 -0600

    added a verbose flag

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main(): 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("Please provide a prompt!")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == '--verbose':
+        verbose_flag = True
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum'
+        contents = messages
     )
 
     print(response.text)
@@ -17,8 +32,12 @@ def main():
     if response.usage_metadata is None or response is None:
         print("Response is Null")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+if __name__ == "__main__":
+    main()
 
-main()
