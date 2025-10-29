fd7d431d66dceb471f167efd7bcd9d0f3fa20673
swaruppanda11 <swaruppanda11@gmail.com>
2025-10-29 16:13:24 -0600

    figured out how to get file info from directory and sub-files. has some bugs that i need to fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info
 
 def main(): 
     load_dotenv()
@@ -39,5 +40,7 @@ def main():
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
-    main()
+    print(get_files_info("calculator"))
+    
+
 
